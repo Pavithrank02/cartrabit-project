76ae732e67828c0845fe3755d0e883dad66a93f2
Pavithran2 <pavithrank02@gmail.com>
2023-04-04 12:40:48 +0530

    Login Card UI

---
+++ guest-room-booking/src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='Card'>
+      <div className='Card_header'>
+        <div className='Card-text-A'>
+          Admin Login
+        </div>
+        <div className='Card-text-C'>
+          Customer Login
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
