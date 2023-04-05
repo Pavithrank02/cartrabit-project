4f8aaa9586c7108f8c077d10eb6e9412c927d6e2
Pavithran2 <pavithrank02@gmail.com>
2023-04-05 18:23:53 +0530

    middleware created

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "mydb3",
+    database: "mydb",
   });
 
  connection.connect(function (error) {
