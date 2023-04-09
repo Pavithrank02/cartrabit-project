08f8912fafe0ddc43d9ca1753d9db92a1827f6c8
Pavithran2 <pavithrank02@gmail.com>
2023-04-09 22:31:38 +0530

    gallery database created

--- server/demo_db_connection.js
+++ server/demo_db_connection.js
@@ -19,25 +19,25 @@ var con = mysql.createConnection({
 //   console.log("Connected!");
 // });
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   var sql = "CREATE TABLE room (id INT AUTO_INCREMENT PRIMARY KEY, room_no VARCHAR(20), Description VARCHAR(100), house_owner VARCHAR(20), beds VARCHAR(15), amount VARCHAR(15), houseNo VARCHAR(15), maxDay VARCHAR(15), minDay VARCHAR(15), images VARCHAR(15), floor VARCHAR(15) )";
-//   con.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("Table created");
-//   });
-// });
-
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "ALTER TABLE room MODIFY startDay DATETIME";
+  var sql = "CREATE TABLE gallery (id INT AUTO_INCREMENT PRIMARY KEY, owner VARCHAR(20), images VARCHAR(100))";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table altered");
+    console.log("Table created");
   });
 });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "ALTER TABLE room MODIFY startDay DATETIME";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table altered");
+//   });
+// });
 
 
 // ALTER TABLE table_name MODIFY column_name varchar(new_length)
