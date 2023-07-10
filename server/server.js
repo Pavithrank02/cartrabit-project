524cf20bce8b34dfec8690b936c490bac3358bcf
Pavithran2 <pavithrank02@gmail.com>
2023-07-10 09:56:24 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -30,10 +30,10 @@ app.get("/", function (req, res) {
 
 // connection configurations
 var dbConn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "mydb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
 });
 // connect to database
 dbConn.connect();
